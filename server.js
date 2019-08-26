90629fca46e133fa3a18c9144651159c87f5a011
Mathias Roggeman <mathias.roggeman@hotmail.be>
2019-08-26 15:49:09 +0200

    Search by opnameplaats_straat working

--- server.js
+++ server.js
@@ -32,3 +32,34 @@ app.get("/list", (req, res) => {
       res.render("overtreding.ejs", { overtredingen: result });
     });
 });
+// Show the search form
+app.get("/search", (req, res) => {
+  res.render("search.ejs", {});
+});
+
+// Find all comments for a post
+app.post("/search", (req, res) => {
+  var opnameplaats_straat = req.body.opnameplaats_straat;
+  console.log(opnameplaats_straat);
+  db.collection("overtredingen")
+    .find()
+    .toArray((err, result) => {
+      if (err) throw err;
+      var stringified = JSON.stringify(result);
+      var Overtredinglist = JSON.parse(stringified);
+
+      //console.log("TCL: Overtredinglist", Overtredinglist);
+      //console.log("TCL: result", result);
+
+      var list = [];
+
+      for (var i = 0; i < Overtredinglist.length; i++) {
+        //  console.log("TCL: Overtredinglist[i]", Overtredinglist[i].opnameplaats_straat);
+        if (Overtredinglist[i].opnameplaats_straat == opnameplaats_straat) {
+          list.push(Overtredinglist[i]);
+        }
+      }
+      console.log(list);
+      res.render("search_result.ejs", { list });
+    });
+});
